d75486b5818dce4f6ebc9e13a3e8d2a1db89e310
Bogdan <bogdan.talpiga@3nyble.com>
2020-11-07 16:07:45 +0200

    Change reload script

--- src/main/resources/documentation/initial-recency-and-frequency-calc.sql
+++ src/main/resources/documentation/initial-recency-and-frequency-calc.sql
@@ -9,7 +9,7 @@ select ca.system_id, ca.consumer_id, max(external_system_date) as recency, count
 from consumer_actions ca join affinity.action_scores acts on ca.action_id = acts.action_id
 where external_system_date >= now()-'2 year'::interval and external_system_date < now() and
     ((ca.id<=:lastRmcActionId and ca.system_id = 1) or (ca.id<=:lastRrpActionId and ca.system_id = 2) )
-group by ca.system_id, ca.consumer_id
+group by ca.system_id, ca.consumer_id;
 
 
 alter table recency_and_frequency_start add column id serial;
