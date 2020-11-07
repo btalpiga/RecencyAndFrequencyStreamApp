d113879d02a945332f1fc1fce17305447c3a09e0
Bogdan <bogdan.talpiga@3nyble.com>
2020-11-07 16:03:14 +0200

    Change reload script

--- src/main/resources/documentation/initial-recency-and-frequency-calc.sql
+++ src/main/resources/documentation/initial-recency-and-frequency-calc.sql
@@ -7,7 +7,8 @@ UPDATE config_parameters set value = date_trunc('milliseconds', now()-'2 year'::
 create table recency_and_frequency_start as
 select ca.system_id, ca.consumer_id, max(external_system_date) as recency, count(1) as frequency
 from consumer_actions ca join affinity.action_scores acts on ca.action_id = acts.action_id
-where external_system_date >= now()-'2 year'::interval and external_system_date < now()
+where external_system_date >= now()-'2 year'::interval and external_system_date < now() and
+    ((ca.id<=:lastRmcActionId and ca.system_id = 1) or (ca.id<=:lastRrpActionId and ca.system_id = 2) )
 group by ca.system_id, ca.consumer_id
 
 
